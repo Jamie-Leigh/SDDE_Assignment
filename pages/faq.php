a073ab7b19e2b3a64277b8e3f01c09491dd27a27
Jamie Leigh <jamie.leigh@bt.com>
2021-12-16 14:33:27 +0000

    update FAQs

--- pages/faq.php
+++ pages/faq.php
@@ -10,8 +10,8 @@
             </h2>
             <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                    We have four UK locations: Bath, Ipswich, London and Newcastle.
-                    If you buy a car online, you can collect it from any of our locations.
+                    We have four UK locations: Bath, Ipswich, London and Newcastle. Our opening hours are 9am-5pm, 7 days a week.
+                    If you hire a car online, you can collect it from any of our locations.
                     <div id='map'></div>
                     <script>
                     mapboxgl.accessToken = 'pk.eyJ1IjoiamFtaWUtbGVpZ2giLCJhIjoiY2t3cGV2Ymx0MDh0bzJ1cnRhcGduNGJqYiJ9.atGN7v9atiuCwDxcz1HCiw';
@@ -29,7 +29,7 @@
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingTwo">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Do I have to sign up to buy a car?
+                Do I have to sign up to hire a car?
             </button>
             </h2>
             <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -41,12 +41,14 @@
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingThree">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                What if I don't like the car after I've bought it? Can I return it?
+                How can I contact you?
             </button>
             </h2>
             <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                    You can return any car within 30 days of buying it, as long as the condition is the same as how we sold it to you.
+                    You can contact us at any time using our email address: <a href="mailto:help@sevent.com">help@sevent.com</a>.
+                    We will aim to get back to you within 2 working days.
+                    You can also ring us on 0123456789, or you can pop into one of our forecourts when we're open (see the first FAQ for details).
                 </div>
             </div>
         </div>
